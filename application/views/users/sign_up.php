bee8c7b95f11738998b25fd206e4b9eb37bad7b4
roy38 <royjim371@gmail.com>
2020-04-27 23:53:54 +0800

    first commit

---
+++ application/views/users/sign_up.php
@@ -0,0 +1,26 @@
+<div class="container" style="margin-top: 200px;">
+  <h2>Register form</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="firstname">Firstname:</label>
+      <input type="text" class="form-control" id="firstname" placeholder="Enter firstname" name="firstname">
+    </div>
+    <div class="form-group">
+      <label for="lastname">lastname:</label>
+      <input type="text" class="form-control" id="lastname" placeholder="Enter lastname" name="lastname">
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    </div>
+    <div class="form-group">
+      <label for="repswd">Re-Password:</label>
+      <input type="password" class="form-control" id="repswd" placeholder="Re-enter password" name="repswd">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
